2842a3920e21c0ca32a84327b5dcbc37d98611aa
ShaneWilmes <shanewilmes@yahoo.com>
2022-05-30 17:43:23 -0600

    started over on schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,27 @@
---CREATING A DATABASE
-DROP DATABASE IF EXISTS employeeTracker_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-CREATE  DATABASE employeeTracker_db;
+USE DATABASE employee_db;
 
-USE employeeTracker_db;
-
---CREATING TABLES FOR DEPARTMENTS, ROLES AND EMPLOYEES
-CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    NAME VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id)
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE employees (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  manager_id INT,
-  role_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
-
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30,)
+    last_name VARCHAR(30,)
+    role_id VARCHAR(30),
+    manager_id INT,
+    PRIMARY KEY (id)
 );
+
