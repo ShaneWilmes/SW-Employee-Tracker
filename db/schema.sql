4d7b8c37ff37b3b341875b27cbe30034574e028b
ShaneWilmes <shanewilmes@yahoo.com>
2022-05-30 14:41:33 -0600

    set up databases

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,31 @@
---Notes for push
+--CREATING A DATABASE
+DROP DATABASE IF EXISTS employeeTracker_db;
+
+CREATE  DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+--CREATING TABLES FOR DEPARTMENTS, ROLES AND EMPLOYEES
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  movie_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
