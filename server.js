f9dcb8667822228d217ea9ae355ed323cb2d8bbc
ShaneWilmes <shanewilmes@yahoo.com>
2022-05-31 14:13:33 -0600

    switch case for user choice

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const mysql = require("mysql2");
 const consoleTable = require("console.table");
 const express = require("express");
 
+// Create a connection and log it to the console
 const connection = mysql.createConnection({
     host: "localhost",
-    port:  3001,
+    port: 3001,
     user: "root",
     password: "1234",
     database: "employee_db"
@@ -17,17 +18,73 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId);
     init();
-  });
+});
 
-  function init() {
-      inquirer.prompt({
-          type: "list",
-          name: "start",
-          message: "Please make a selection from the following:",
-          choices: ["View All Employees", "View All Departments", "View All Roles", "View All Employees By Department", "View All Employees By Manager",
-          "Add Employee", "Remove Employee", "Update Employee Role", "Add Employee Role", "Remove Role", "Add New Department", "Remove Department"]
-      })
+// App initialization and prompting user to make a choice
+function init() {
+    inquirer.prompt({
+        type: "list",
+        name: "start",
+        message: "Please make a selection from the following:",
+        choices: ["View All Employees", "View All Departments", "View All Roles", "View All Employees By Department", "View All Employees By Manager",
+            "Add Employee", "Remove Employee", "Update Employee Role", "Add Employee Role", "Remove Role", "Add New Department", "Remove Department"]
+    })
 
-        
-  };
-  
+        .then(function (response) {
+            switch (response.start) {
+
+
+                case "View All Employees":
+                    displayEmployees();
+                    break;
+
+                case "View All Departments":
+                    viewDepartments();
+                    break;
+                
+                case "View All Roles":
+                    viewRoles();
+                    break;
+                
+                case "View All Employees by Department":
+                    displayEmpDept();
+                    break;
+
+                case "View All Employees by Manager":
+                    displayEmpMgr();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+
+                case "Update Employee Role":
+                    updateRole();
+                    break;
+
+                case "Remove Employee Role":
+                    removeRole();
+                    break;
+
+                case "Add New Department":
+                    addDepartment();
+                    break;
+
+                case "Remove Department":
+                    removeDepartment();
+                    break;
+
+                case "Update Employee Manager":
+                    updateManager();
+                    break;
+            }
+        })
+
+
+};
+
+
