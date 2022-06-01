a6eae638c985f19040aad879c161c0dca3c20057
ShaneWilmes <shanewilmes@yahoo.com>
2022-06-01 10:29:58 -0600

    wrote display employee by dept function

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
 const express = require("express");
+const { response } = require("express");
 
 // Create a connection and log it to the console
 const connection = mysql.createConnection({
@@ -124,6 +125,34 @@ function viewRoles(){
       })
 }
 
+// View employees by department
+function displayEmpDept() {
+    const depQuery1 = ('SELECT * FROM department');
+    connection.query(depQuery1, (err, response)=> {
+        if (err) throw err;
+        const departments = response.map(element => {
+            return { name: `${element.name}`}
+        });
+
+        inquirer.prompt([{
+            type: "list",
+            name: "dept",
+            message: "Please select a department and to view employees",
+            choices: departments
+
+        }]).then(answer => {
+            const depQuery2 = `SELECT employee.first_name, employee.last_name, employee.role_id AS role, CONCAT(manager.first_name,' ',manager.last_name) AS manager, department.name as department 
+            FROM employee LEFT JOIN role on employee.role_id = role.id 
+            LEFT JOIN department ON role.department_id =department.id LEFT JOIN employee manager ON employee.manager_id=manager.id
+            WHERE ?`
+            connection.query(depQuery2, [{ name: answer.dept }], function (err, res) {
+                if (err) throw err;
+                console.table(res)
+                init();
+            })
+        })    
+    })
+};
 
 
 
